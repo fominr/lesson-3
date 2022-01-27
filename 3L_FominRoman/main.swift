8fa79c43d3c7f24a72555c2cdd2464cf0dc60920
romanf <fomin.tp21@mail.ru>
2022-01-27 15:47:20 +0300

    перечисления

--- 3L_FominRoman/main.swift
+++ 3L_FominRoman/main.swift
@@ -7,5 +7,31 @@
 
 import Foundation
 
-print("Hello, World!")
+// 1. Описать несколько структур – любой легковой автомобиль SportCar и любой грузовик TrunkCar.
 
+enum EngineStatus {  // состояние двигателя
+    case stop
+    case start
+    }
+
+enum WindowSatus {   // состояние окон.
+    case open
+    case close
+}
+
+enum BagageStatus {  // погрузка или разгрузка авто
+    case load
+    case unload
+}
+
+
+//struct TrunkCar {
+//    let brand: String
+//    let power: Int
+//    let year: Int
+//    let bagageMax: Int
+//    var bagageWeight: Int
+//    var cargo: Int
+//    var engineStart: EngineStatus
+//    var windowOpen: WindowSatus
+//    var bagage: BagageStatus
