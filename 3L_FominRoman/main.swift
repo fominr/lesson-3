af9f93d4e4e4c40d94f3241f06b42fd3d340788d
romanf <fomin.tp21@mail.ru>
2022-01-27 16:52:15 +0300

    описание структуры грузового и спорт автомобилей

--- 3L_FominRoman/main.swift
+++ 3L_FominRoman/main.swift
@@ -1,9 +1,4 @@
-//
-//  main.swift
-//  3L_FominRoman
-//
-//  Created by Rom on 27.01.2022.
-//
+
 
 import Foundation
 
@@ -17,21 +12,40 @@ enum EngineStatus {  // состояние двигателя
 enum WindowSatus {   // состояние окон.
     case open
     case close
-}
+    }
 
 enum BagageStatus {  // погрузка или разгрузка авто
     case load
     case unload
+    }
+
+enum TransmissionStatus {   // состояние трансмиссии
+    case parking
+    case neutral
+    case drive
+    }
+
+
+//2. Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, запущен ли двигатель, открыты ли окна, заполненный объем багажника.
+struct TrunkCar {
+    let brand: String
+    let power: Int
+    let year: Int
+    let bagageMax: Int
+    var bagageWeight: Int
+    var cargo: Int
+    var engineStart: EngineStatus
+    var windowOpen: WindowSatus
+    var bagage: BagageStatus
 }
 
-
-//struct TrunkCar {
-//    let brand: String
-//    let power: Int
-//    let year: Int
-//    let bagageMax: Int
-//    var bagageWeight: Int
-//    var cargo: Int
-//    var engineStart: EngineStatus
-//    var windowOpen: WindowSatus
-//    var bagage: BagageStatus
+struct  sportCar{
+        let brand: String
+        let year: Int
+        let weightCar: Int  // вес авто
+        let weightTotal: Int    // общий вес: авто + груз
+        var weightBagage: Int   // вес груза
+        let window: WindowSatus
+        var engine: EngineStatus
+        var transmission: TransmissionStatus
+}
